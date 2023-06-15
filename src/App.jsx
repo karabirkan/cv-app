e79b72b82c3bdeb20e4bd2356cdfd6cc070ad249
karabirkan <115654236+tokyoparis@users.noreply.github.com>
2023-06-15 11:21:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Cv from "./Cv";
+function App() {
+  return (
+    <div>
+      <Cv />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cv.jsx
@@ -0,0 +1,8 @@
+export default function Cv() {
+  return (
+    <div>
+      <p>Name: </p>
+      <p>Experience: </p>
+    </div>
+  );
+}
